9816fed5aa592130c7801977df337297eeb48f3b
moroz-art-dev <moroz.art.dev@gmail.com>
2021-10-27 17:58:28 +0300

    resolve

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import Post from "./Post"
-//import json from "./assets/json.json"
+import Post from "@models/Post"
+//import json from "./assets/json"
 //import xml from "./assets/data.xml"
 //import csv from "./assets/data.csv"
-import WebpackLogo from "./assets/webpack-logo.png"
+import WebpackLogo from "@/assets/webpack-logo.png"
 import "./styles/styles.css"
 
 const post = new Post('Webpack Post Title', WebpackLogo)
