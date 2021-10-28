5f10a38fac3813551085a06f0b2b2b13086a2fa7
moroz-art-dev <moroz.art.dev@gmail.com>
2021-10-28 23:57:35 +0300

    copyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -33,7 +34,15 @@ module.exports = {
             //title: 'Webpack Art', при template параметр не работает
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/favicon.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                 }
+            ]
+        })
     ],
     module: {
         rules: [
